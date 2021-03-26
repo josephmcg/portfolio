9a9b572730fa6c61b5e0440847820de2c5ea0502
Joe McGrath <josephmcg01@gmail.com>
2021-03-26 17:00:28 +0900

    formatting files

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,9 @@
 module.exports = {
   siteMetadata: {
     title: `Joe McGrath`,
-    description: "test",
+    description:
+      "Joe is a Tokyo-based web developer. He uses React, JavaScript, CSS, and HTML to develop performant and intuitive websites.",
+    author: `@josephmcg`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
