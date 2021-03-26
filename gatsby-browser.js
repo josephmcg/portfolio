625ffd346f907b458d6f2312bc0262140fe22555
Joe McGrath <josephmcg01@gmail.com>
2021-03-26 18:47:44 +0900

    safari smoothscroll polyfill and package audit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,7 @@
+// Prevent Gatsby from throwing build errors
 import Bootstrap from "bootstrap"
 import "@popperjs/core/dist/umd/popper.min.js"
+
+// Safari smoothscroll polyfill
+import smoothscroll from "smoothscroll-polyfill"
+smoothscroll.polyfill()
