f464c3e80a6e97a9d4896362e60f0cbc106e46da
Joe McGrath <josephmcg01@gmail.com>
2024-10-10 19:19:44 +0900

    refactor: more performant drag

--- src/components/Polygons.tsx
+++ src/components/Polygons.tsx
@@ -2,50 +2,16 @@
 
 import { useEffect, useState } from 'react'
 
-interface PolygonProps extends React.ComponentProps<'div'> {
+interface PolygonProps {
   size: number
   xPos: number
   yPos: number
   rotation: number
-  handleDrag: (index: number, xPos: number, yPos: number) => void
   index: number
 }
 
-const Polygon: React.FC<PolygonProps> = ({
-  size,
-  xPos,
-  yPos,
-  rotation,
-  handleDrag,
-  index,
-  ...props
-}) => {
-  const handleMouseDown = (event: React.MouseEvent): void => {
-    const offset = {
-      x: event.clientX - xPos,
-      y: event.clientY - yPos,
-    }
-
-    const handleMouseMove = (moveEvent: MouseEvent): void => {
-      handleDrag(
-        index,
-        moveEvent.clientX - offset.x,
-        moveEvent.clientY - offset.y,
-      )
-    }
-
-    const handleMouseUp = (): void => {
-      document.removeEventListener('mousemove', handleMouseMove)
-      document.removeEventListener('mouseup', handleMouseUp)
-    }
-
-    // Attach event listeners to the document to handle dragging across the entire screen
-    document.addEventListener('mousemove', handleMouseMove)
-    document.addEventListener('mouseup', handleMouseUp)
-  }
-
+const Polygon: React.FC<PolygonProps> = ({ size, xPos, yPos, rotation }) => {
   return (
-    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
     <div
       className="absolute animate-float cursor-grab bg-white bg-opacity-10 active:cursor-grabbing"
       style={{
@@ -55,20 +21,20 @@ const Polygon: React.FC<PolygonProps> = ({
         left: `${String(xPos)}px`,
         rotate: `${String(rotation)}deg`,
       }}
-      onMouseDown={handleMouseDown}
-      {...props}
     />
   )
 }
 
 export const Polygons: React.FC = () => {
-  const [polygons, setPolygons] = useState<Omit<PolygonProps, 'handleDrag'>[]>(
-    [],
-  )
+  const [polygons, setPolygons] = useState<PolygonProps[]>([])
+  const [activePolygon, setActivePolygon] = useState<number>()
+  const [offset, setOffset] = useState({ x: 0, y: 0 })
+
+  const isNoActivePolygon = typeof activePolygon !== 'number'
 
   useEffect(() => {
     const createPolygons = (): void => {
-      const newPolygons: Omit<PolygonProps, 'handleDrag'>[] = []
+      const newPolygons: PolygonProps[] = []
 
       for (let i = 0; i < 20; i++) {
         const size = Math.random() * 50 + 50 // Random size between 50px and 100px
@@ -91,28 +57,73 @@ export const Polygons: React.FC = () => {
     createPolygons()
   }, [])
 
-  const handleDrag = (index: number, xPos: number, yPos: number): void => {
+  const handleMouseDown = (index: number, event: React.MouseEvent): void => {
+    const polygon = polygons[index]
+    if (!polygon) {
+      return
+    }
+
+    setActivePolygon(index)
+
+    setOffset({
+      x: event.clientX - polygon.xPos,
+      y: event.clientY - polygon.yPos,
+    })
+  }
+
+  const handleMouseMove = (event: MouseEvent): void => {
+    if (isNoActivePolygon) {
+      return
+    }
+
+    const xPos = event.clientX - offset.x
+    const yPos = event.clientY - offset.y
+
     setPolygons((prevPolygons) =>
       prevPolygons.map((polygon, i) =>
-        i === index ? { ...polygon, xPos, yPos } : polygon,
+        i === activePolygon ? { ...polygon, xPos, yPos } : polygon,
       ),
     )
   }
 
+  const handleMouseUp = (): void => {
+    setActivePolygon(undefined)
+  }
+
+  useEffect(() => {
+    if (isNoActivePolygon) {
+      document.removeEventListener('mousemove', handleMouseMove)
+      document.removeEventListener('mouseup', handleMouseUp)
+    } else {
+      document.addEventListener('mousemove', handleMouseMove)
+      document.addEventListener('mouseup', handleMouseUp)
+    }
+
+    return (): void => {
+      document.removeEventListener('mousemove', handleMouseMove)
+      document.removeEventListener('mouseup', handleMouseUp)
+    }
+  }, [activePolygon])
+
   return (
     <div className="absolute inset-0 overflow-hidden">
       {polygons.map((polygon, index) => (
-        <Polygon
+        // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+        <div
           key={index}
-          size={polygon.size}
-          xPos={polygon.xPos}
-          yPos={polygon.yPos}
-          rotation={polygon.rotation}
-          handleDrag={handleDrag}
-          index={index}
-        />
+          onMouseDown={(event) => {
+            handleMouseDown(index, event)
+          }}
+        >
+          <Polygon
+            size={polygon.size}
+            xPos={polygon.xPos}
+            yPos={polygon.yPos}
+            rotation={polygon.rotation}
+            index={polygon.index}
+          />
+        </div>
       ))}
     </div>
   )
 }
-;``
